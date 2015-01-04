09dcda2e781a831d52091cb549726eab055c7dd3
Etienne Theodore <etiennetheodore@pc2.home>
2015-01-04 22:24:11 +0100

    go

--- main.c
+++ main.c
@@ -1,29 +1,25 @@
 #include "./includes/ft_sh1.h"
 
-void	ft_exe_cmd(char *cmd, char **av, char** ev)
+int	main()
 {
-	if (ft_strequ(cmd, "exit"))
-		exit(1);
-	if (ft_strequ(cmd, "ls"))
-		execve("/bin/ls", av, ev);
-}
+	char	**arg;
+	char	*input;
+	pid_t	work;
 
-int 	main(int ac, char **av, char **ev)
-{
-	pid_t	father;
-	char *line;
-
-	father = fork();
-	if (father > 0)
-		wait(0);
-	if (father == 0)
+	while (42)
 	{
-		while (42)
+		ft_putstr("$> ");
+		work = fork();
+		if (work > 0)
+			wait(NULL);
+		if (work == 0)
 		{
-			ft_putstr("$> ");
-			if (get_next_line(1, &line) == -1)
-				exit(1);
-			ft_exe_cmd(line, av, ev);
+			get_next_line(1, &input);
+			arg = ft_strsplit(input, ' ');
+			// if (ft_strequ(arg[0], "exit"));
+			// 	exit(EXIT_SUCCESS);
+			input = ft_strjoin("/bin/", arg[0]);
+			execve(input, arg, NULL);
 		}
 	}
 	return (0);
