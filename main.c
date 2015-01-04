b3e4ce2ae23784e2e555e62e281dfe4903e7167d
Etienne Theodore <etiennetheodore@pc2.home>
2015-01-05 00:08:04 +0100

    add cd

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include "./includes/ft_sh1.h"
 
+void	ft_exit(char **arg, pid_t work)
+{
+	if (ft_strequ(arg[0], "exit"))
+		kill(work, SIGKILL);	
+}
+
+void	ft_command_not_found(int ret, char **arg)
+{
+	if (ret == -1 && !ft_strequ(arg[0], "\n") && !ft_strequ(arg[0], NULL) &&
+		!ft_strequ(arg[0], "clear") && !ft_strequ(arg[0], "cd"))
+	{
+		ft_putstr_fd(arg[0], 2);
+		ft_putendl_fd(": command not found", 2);
+	}
+}
+
 void	ft_clear(char **av, char **ev)
 {
 	if(fork() == 0) 
@@ -9,6 +25,42 @@ void	ft_clear(char **av, char **ev)
 	} 
 	else
 		wait(NULL);
+}
+
+void	ft_cd(char **arg)
+{
+	char *buf;
+	char *ret;
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	buf = (char*)ft_memalloc(sizeof(char) * 256);
+	buf = getcwd(buf, 256);
+	while (buf[i])
+		i++;
+	while (i > j && buf[i] != '/')
+		i--;
+	i++;
+	ret = (char*)ft_memalloc(sizeof(char) * i + 1);
+	while (j < i)
+	{
+		ret[j] = buf[j];
+		j++;
+	}
+	ft_putstr(ret);
+	if (ft_strequ(arg[1], ".."))
+	{
+		chdir(ret);
+	}
+/*	if(fork() == 0) 
+	{
+		execve("/usr/bin/cd", av, ev);
+		exit(1);
+	} 
+	else
+		wait(NULL);*/
 }
 
 int	main(int ac, char **av, char **ev)
@@ -30,18 +82,13 @@ int	main(int ac, char **av, char **ev)
 		{
 			get_next_line(1, &input);
 			arg = ft_strsplit(input, ' ');
-			if (ft_strequ(arg[0], "exit"))
-				kill(work, SIGKILL);
+			ft_exit(arg, work);
 			input = ft_strjoin("/bin/", arg[0]);
-			ret = execve(input, arg, NULL);
+			ft_command_not_found(execve(input, arg, ev), arg);
 			if (ft_strequ(arg[0], "clear"))
 				ft_clear(av, ev);
-			if (ret == -1 && !ft_strequ(arg[0], "\n") && !ft_strequ(arg[0], NULL))
-			{
-				ft_putstr("-bash: ");
-				ft_putstr(arg[0]);
-				ft_putendl(": command not found");
-			}
+			if (ft_strequ(arg[0], "cd"))
+				ft_cd(arg);
 		}
 	}
 	return (0);
